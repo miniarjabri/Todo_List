eed598da4cbec338e924d1fa0abf39927642bbad
Miniar JABRI <123066240+miniarjabri@users.noreply.github.com>
2025-03-12 21:32:25 +0100

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Home Page
-router.get('/', (req, res) => {
-  res.render('index', { user: req.user });
-});
-
-module.exports = router;
